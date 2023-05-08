15666a60232064550b1461966817aba3a2b95fb6
Charley Reeson <neon.fortytwo@gmail.com>
2023-05-08 21:43:12 +0100

    add consumables

--- app/Models/Creature.php
+++ app/Models/Creature.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Creature extends Model
 {
@@ -13,5 +14,9 @@ class Creature extends Model
 
     public function family(): BelongsTo {
         return $this->belongsTo(Family::class);
+    }
+
+    public function consumable(): HasOne {
+        return $this->hasOne(Consumable::class, 'id', 'component');
     }
 }

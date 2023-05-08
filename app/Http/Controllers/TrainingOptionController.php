04e0898bcc80876387b9ebb205f3b9d64e3adff0
Charley Reeson <neon.fortytwo@gmail.com>
2023-05-08 22:51:41 +0100

    import training options

---
+++ app/Http/Controllers/TrainingOptionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrainingOption;
+use Illuminate\Http\Request;
+
+class TrainingOptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TrainingOption $trainingOption)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TrainingOption $trainingOption)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TrainingOption $trainingOption)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TrainingOption $trainingOption)
+    {
+        //
+    }
+}
---
+++ database/seeders/TrainingOptions.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
+class TrainingOptions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //        
+        $sql = '/var/www/html/database/seeders/trainingoptions.sql';
+          
+        $db = [
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'host' => env('DB_HOST'),
+            'database' => env('DB_DATABASE')
+        ];
+  
+        exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
+  
+        Log::info('SQL Import Done');
+    }
+}

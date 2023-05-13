65f2ffae89ce79bca16b84053f76a347b6d4a6e1
Charley Reeson <neon.fortytwo@gmail.com>
2023-05-13 01:20:27 +0100

    automate menu links

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,43 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="motion-safe:scroll-smooth motion-reduce:scroll-auto">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ $page['title'] ?? 'Unnamed Page' }}</title>
-        @vite('resources/css/app.css')
-        @stack('css')
-        @stack('scripts')
-    </head>
-    <body class="antialiased bg-uc-blue font-sans font-normal font-base">
-        <div class='max-w-5xl m-auto drop-shadow-[0px_0px_8px_rgba(200,204,179,0.9)]'>
-            <header>
-                <div
-                    id='banner'
-                    role='presentation'
-                    class="h-[232px] bg-[url('../../resources/css/images/bannerv2_clean.png')] bg-no-repeat bg-center">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $page['title'] ?? 'Unnamed Page' }}</title>
+    @vite('resources/css/app.css')
+    @stack('css')
+    @stack('scripts')
+</head>
+
+<body class="antialiased bg-uc-blue font-sans font-normal font-base">
+    <div class='max-w-5xl m-auto drop-shadow-[0px_0px_8px_rgba(200,204,179,0.9)]'>
+        <header>
+            <div id='banner' role='presentation' class="h-[232px] bg-[url('../../resources/css/images/bannerv2_clean.png')] bg-no-repeat bg-center">
+            </div>
+            <div class='drop-shadow-lg flex flex-row items-center justify-between bg-uc-mdbrown p-4'>
+                <div>
+                    UC Archive
                 </div>
-                <div class='drop-shadow-lg flex flex-row items-center justify-between bg-uc-mdbrown p-4'>
-                    <div>
-                        UC Archive
-                    </div>
-                    <nav id='menu' class='grow'>
-                        <ul class='flex flex-row flex-wrap justify-end text-uc-dkbrown uppercase text-sm font-medium'>
-                            <li class='p-2'>Home</li>
-                            <li class='p-2'>Creatures</li>
-                            <li class='p-2'>Items</li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-            {{-- 
+                <nav id='menu' class='grow'>
+                    <ul class='flex flex-row flex-wrap justify-end text-uc-dkbrown uppercase text-sm font-medium'>
+                        @foreach ([
+                            ['home', 'home'],
+                            ['creatures', 'Creatures']
+                        ] as $route)
+                            <li class='p-2'><a href="{{ route($route[0]) }}">{{$route[1]}}</a></li>
+                        @endforeach
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        {{--
             @if (isset($page['route']))
                 <section id='breadcrumb' aria-label="Breadcrumb" class='bg-uc-ltbrown text-xs'>
                     {{ Breadcrumbs::render($page['route'], $page['name']) }}
-                </section>
-            @endif --}}
-            <main class='bg-gradient-to-b from-uc-grad-begin to-uc-grad-end bg-uc-grad-end bg-[length:100%_526px] bg-no-repeat p-5'>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+        </section>
+        @endif --}}
+        <main class='bg-gradient-to-b from-uc-grad-begin to-uc-grad-end bg-uc-grad-end bg-[length:100%_526px] bg-no-repeat p-5'>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::prefix('creatures')->group(function() {
-    Route::controller(FamilyController::class)->group(function() {
+Route::prefix('creatures')->group(function () {
+    Route::controller(FamilyController::class)->group(function () {
         /**
          * Create a new family
          */
@@ -31,15 +31,15 @@ Route::prefix('creatures')->group(function() {
         /**
          * Show all families
          */
-        Route::get('/all', 'index')->name('all families');
-        
-        Route::prefix('{family}')->where(['family', '[a-zA-Z]+'])->group(function(){
+        Route::get('/all', 'index')->name('creatures');
+
+        Route::prefix('{family}')->where(['family', '[a-zA-Z]+'])->group(function () {
             /** 
              * Show a family of creatures
              */
             Route::get('/', 'show')->name('family');
-        
-            Route::controller(CreatureController::class)->group(function(){
+
+            Route::controller(CreatureController::class)->group(function () {
                 /**
                  * Add a new creature to the family
                  */
@@ -58,14 +58,14 @@ Route::prefix('creatures')->group(function() {
      * Or in the case of ambiguous matches, return the matches and let the user
      * decide where to go.
      */
-    Route::get('/{name}', function(Request $request, string $name){
+    Route::get('/{name}', function (Request $request, string $name) {
         // There are some creatures whose names are not unique, such as 'Egg'!
         // This means if one of these is provided, we should instead
         // give the user several results.
         // But if only one match is found, we can just redirect the user to the correct
         // page
         $results = Creature::where('name', $name)->get();
-        if($results->count() > 1){
+        if ($results->count() > 1) {
             return $results->all();
         }
 

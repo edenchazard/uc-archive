10fb9fd892e3ef2030b3656b193958ce00c35993
Charley Reeson <neon.fortytwo@gmail.com>
2023-05-14 10:46:55 +0100

    improve evo display

--- resources/views/components/evolutionary-line.blade.php
+++ resources/views/components/evolutionary-line.blade.php
@@ -1,11 +1,26 @@
-<ol {{ $attributes->merge(['class' => 'flex flex-row flex-wrap items-stretch list-none gap-2 evolutionary-line']) }}>
-    @foreach ($stages as $pet)
-        <li class="flex flex-col justify-between items-center">
-            <span>{{ $pet->creature->name }}</span>
-            <img src='{{ $pet->imageLink() }}'
-                alt="{{ $pet->creature->name }}" 
-                class='creature-image' />
-        </li>
-        <li aria-hidden="true" role="presentation" class="flex flex-col items-center justify-center last:hidden">→</li>
-    @endforeach
-</ol>
+<div>
+    <div {{ $attributes->merge(['class' => 'grid flex-row items-stretch auto-cols-[minmax(min-content,max-content)] gap-x-2']) }}>
+        @foreach ($stages as $pet)
+            <div class="flex justify-center flex-1 row-start-1">
+                <a class='self-center' href="{{ route('creature', [$pet->creature->family->name, $pet->creature->name]) }}">
+                <img src='{{ $pet->imageLink() }}'
+                    alt="{{ $pet->creature->name }}" 
+                    class='self-center creature-image' />
+                </a>
+            </div>
+            <span class="row-start-2 text-center text-ellipsis overflow-hidden whitespace-nowrap">{{ $pet->creature->name }}</span>
+            <span class="hidden row-start-2 sm:block last:hidden">&nbsp;</span>
+            <span aria-hidden="true" role="presentation" class="hidden row-start-1 self-center sm:block last:hidden">→</span>
+        @endforeach
+    </div>
+</div>
+
+@once
+    @push('css')
+<style>
+.evolution:last-child .evolution-arrow{
+    display: none;
+}
+</style>
+    @endpush
+@endonce
--- resources/views/creatures/all-families.blade.php
+++ resources/views/creatures/all-families.blade.php
@@ -1,9 +1,9 @@
 <x-page :data='$page'>
     <section id='list' class='flex gap-x-10 items-center'>
-        Jump to section: 
+        Jump to section:
         <nav class="tracking-wider line text-xl leading-10">
             @foreach ($groups as $category => $families)
-                <a href='#{{$category}}'>{{$category}}</a>
+            <a href='#{{$category}}'>{{$category}}</a>
             @endforeach
         </nav>
         {{--<span>
@@ -13,42 +13,42 @@
     </section>
     <section class="mt-3">
         @foreach ($groups as $category => $families)
-            <div class="relative">
-                <div class='sticky top-0 bg-uc-blue p-1 flex flex-row justify-between'>
-                    <h2 id='{{$category}}' >
-                        <a href='#{{$category}}' class='text-white'>{{$category}}</a>
-                    </h2>
-                    <a href="#list" aria-label="back to start of list" class='sticky p-1 bg-uc-mdbrown rounded-sm'>top</a>
-                </div>
-                <ol class="list-none">
-                    <li >
-                        <ol>
-                            @foreach ($families as $family)
-                                <li class="my-10">
-                                    <div class="flex flex-row gap-2 items-center">
-                                        <h3>{{$family->name}}</h3>
-                                        <x-article-link :url='route("family", $family->name)' />
-                                    </div>
-                                    <x-evolutionary-line :stages="$family->stages" />
-                                </li>
-                            @endforeach
-                        </ol>
-                    </li>
-                </ol>
+        <div class="relative">
+            <div class='sticky top-0 bg-uc-blue p-1 flex flex-row justify-between'>
+                <h2 id='{{$category}}'>
+                    <a href='#{{$category}}' class='text-white'>{{$category}}</a>
+                </h2>
+                <a href="#list" aria-label="back to start of list" class='sticky p-1 bg-uc-mdbrown rounded-sm'>top</a>
             </div>
-            
+            <ol class="list-none">
+                <li>
+                    <ol>
+                        @foreach ($families as $family)
+                        <li class="my-10">
+                            <div class="flex flex-row gap-2 items-center">
+                                <h3 class="text-lg">{{$family->name}}</h3>
+                                <x-article-link :url='route("family", $family->name)' />
+                            </div>
+                            <x-evolutionary-line :stages="$family->stages" />
+                        </li>
+                        @endforeach
+                    </ol>
+                </li>
+            </ol>
+        </div>
+
         @endforeach
     </section>
 </x-page>
 
 @push('scripts')
-    <script type="module">
-document.querySelector('#show-images').addEventListener('change', function(){
-    const action = this.checked ? 'add' : 'remove';
-    console.log(action);
-    document.querySelectorAll('.creature-image').forEach(img => {
-        const action = this.classList[action]('display');
+<script type="module">
+    document.querySelector('#show-images').addEventListener('change', function() {
+        const action = this.checked ? 'add' : 'remove';
+        console.log(action);
+        document.querySelectorAll('.creature-image').forEach(img => {
+            const action = this.classList[action]('display');
+        });
     });
-});
-    </script>
+</script>
 @endpush

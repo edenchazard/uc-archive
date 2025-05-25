0e21def6fdee63dc924c9450394c924ad3f3b933
Charley Reeson <neon.fortytwo@gmail.com>
2025-05-26 00:32:37 +0100

    ecs

--- database/migrations/2025_05_25_135829_stringify_unique_rating.php
+++ database/migrations/2025_05_25_135829_stringify_unique_rating.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Models\Family;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-    private const RATINGS = [
-        0 => null,
-        1 => 'Exotic',
-        2 => 'Legendary',
-    ];
-
     /**
      * Run the migrations.
      */

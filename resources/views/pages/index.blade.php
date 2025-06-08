d9dcfa87393faf67fe807fd80c0fc9da8defd08b
Charley Reeson <neon.fortytwo@gmail.com>
2025-06-08 18:41:35 +0100

    update home info

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,9 +5,9 @@
       of the
       content.</p>
     <h2>I have missing content!</h2>
-    <p>Most content is lost to time, so if you have any missing content, please consider contributing to
-      the
-      archive.</p>
+    <p>
+      A lot of content is lost to time, so if you have any missing content, please consider contributing to the archive.
+    </p>
   </section>
   <section class="mt-2">
     <h2>Archives</h2>
@@ -18,5 +18,28 @@
       <li><a href="{{ route('components.index') }}">Components</a></li>
       <li><a href="{{ route('items.index') }}">Items</a></li>
     </ol>
+  </section>
+  <section class="max-w-prose">
+    <h2>Disclaimer</h2>
+    <div class="space-y-2">
+      <p>
+        The original site was <a href="https://unicreatures.com">unicreatures.com</a> and is no longer
+        online. You can find out more about that at <a
+          href="https://www.deviantart.com/unicreatures/journal/The-Death-of-the-Uni-sites-583992384"
+        >The
+          Death of the Uni sites</a>, a journal entry by a fan, created at the time of its closure.
+      </p>
+      <p>No guarantee of accuracy is given to any of the content. Some information is forever lost, and while efforts
+        have been made to re-establish or verify that information, some remains incomplete.</p>
+      <p>
+        This site makes no claim of ownership to any of the content and is provided for educational and archival
+        purposes only.</p>
+      <p>
+        Text and images are copyright their respective
+        owners. You should seek out the original creator if you wish to reuse content. Where possible, credit to the
+        original creator has been provided on each page. If a credit is missing or incorrectly attributed, please
+        get in touch.
+      </p>
+    </div>
   </section>
 </x-page>

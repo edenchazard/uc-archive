e21d9721422b39c1d03a934abc710389069e75bc
edenchazard <neon.fortytwo@gmail.com>
2026-05-03 22:14:03 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,21 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default ({ mode }) => {
-    const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    return defineConfig({
-        base: env.VITE_BASE_URL,
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: true,
-            }),
-            tailwindcss(),
-        ],
-        server: {
-            hmr: {
-                host: 'localhost',
-            },
-        },
-    });
+  return defineConfig({
+    base: env.VITE_BASE_URL,
+    plugins: [
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      hmr: {
+        host: 'localhost',
+      },
+    },
+  });
 };
